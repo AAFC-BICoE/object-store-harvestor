a0912818c6c9c0279496dfc8f03877ce64fe356d
Dmitri Karpinski <dimitri.karpinski@canada.ca>
2021-03-07 08:58:48 -0500

    More git ignore plus no need to demonize container

--- run_docker_containers.sh
+++ run_docker_containers.sh
@@ -8,4 +8,4 @@ docker_repo_target="aafc-bicoe/object-store-harvestor-local"
 # building docker image
 docker build -t $docker_repo_target -f deployment-local/Dockerfile .
 # bring up/update containers
-docker-compose -f deployment-local/docker-compose.yml up -d
+docker-compose -f deployment-local/docker-compose.yml up
