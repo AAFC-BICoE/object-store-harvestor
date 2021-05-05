c638b92fa65b2ed259e2674fab3dbcc72d7aacd7
kardecom <77641097+kardecom@users.noreply.github.com>
2021-05-05 09:54:06 -0400

    44 harvestor config yml doc (#54)
    * Added harvestor_config.ym doc
    * Fixing typo

--- harvestor/config/app_test.go
+++ harvestor/config/app_test.go
@@ -2,25 +2,8 @@ package config
 
 import (
 	"github.com/stretchr/testify/assert"
-	"regexp"
 	"testing"
 )
-
-func TestGetRelease(t *testing.T) {
-	var file = "../harvestor_config.yml"
-	Load(file)
-	conf := GetConf()
-	assert.Regexp(t, regexp.MustCompile(`^[0-9]{1,3}\.[0-9]{1,3}$`), conf.App.GetRelease())
-
-}
-
-func TestGetName(t *testing.T) {
-	var file = "../harvestor_config.yml"
-	Load(file)
-	conf := GetConf()
-	want := "harvestor"
-	assert.Equal(t, want, conf.App.GetName())
-}
 
 func TestGetEnvironment(t *testing.T) {
 	var file = "../harvestor_config.yml"
